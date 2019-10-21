aff668c8160035244b7e1b8d120a99329660ab6a
Janusz Stal <janusz.stal@uek.krakow.pl>
2019-10-21 02:34:22 +0200

    module 05 finished

---
+++ 05-ModularProgramming/square.py
@@ -0,0 +1,17 @@
+import turtle
+
+pen = turtle.Turtle()
+
+pen.forward(100)
+pen.right(90)     # Rotate clockwise by 90 degrees
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+turtle.done()
