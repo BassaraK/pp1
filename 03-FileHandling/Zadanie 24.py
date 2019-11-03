6b2e6dd45d09bbc35c9ad19d85ca7239c2359f4b
unknown <kacper.bassara@gmail.com>
2019-11-03 22:33:14 +0100

    03 update

--- 03-FileHandling/Zadanie 24.py
+++ 03-FileHandling/Zadanie 24.py
@@ -1,2 +1,10 @@
-tab = [['Marek','Zelnik','zelnik@sed.pl'],['Ewa','Maj','maje@wp.pl'],['Piotr','Wyga','wygap@gop.pl']]
-print(tab)
+tab = [['Imie','Nazwisko','E-mail'],['Marek','Zelnik','zelnik@sed.pl'],['Ewa','Maj','maje@wp.pl'
+],['Piotr','Wyga','wygap@gop.pl']]
+
+with open('zadanie_24.csv','w') as file:
+    for n in tab:
+        for i in n:
+            if i!=n[-1]:
+                file.write(f'{i},')
+            else:
+                file.write(f'{i}\n')
